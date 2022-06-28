40091e0683810ee1eed83c52c1b96fa74651139b
Varun Thakkar <v00thakkar@gmail.com>
2022-06-28 22:39:12 +0530

    Refactored the code to a simpler form

--- dpk.js
+++ dpk.js
@@ -3,25 +3,13 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = event?.partitionKey || event || TRIVIAL_PARTITION_KEY;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH || (event && !event.partitionKey)) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
   return candidate;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const {deterministicPartitionKey} = require("./dpk");
 
 console.log(deterministicPartitionKey());
+console.log(deterministicPartitionKey({partitionKey:"This is event with partition key"}))
+console.log(deterministicPartitionKey({notPartitionKey:"This is event with partition key"}))
